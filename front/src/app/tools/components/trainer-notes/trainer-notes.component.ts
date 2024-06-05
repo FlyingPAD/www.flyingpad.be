09060c7f17c37a9054b2bfe8fad2e07b7ffaebb8
FlyingPAD <99176621+FlyingPAD@users.noreply.github.com>
2024-06-05 15:40:45 +0200

    Update Trainer

--- front/src/app/tools/components/trainer-notes/trainer-notes.component.ts
+++ front/src/app/tools/components/trainer-notes/trainer-notes.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AudioOldService } from '../../../core/services/audio-old.service';
+
+
+export class Note
+{
+  name : string = ''
+  freq : number = 0
+}
 
 @Component({
   selector: 'app-trainer-notes',
@@ -7,6 +15,7 @@ import { Component } from '@angular/core';
 })
 export class TrainerNotesComponent 
 {
+  audioService = inject(AudioOldService)
   gameConfig : boolean = false
   gameStart : boolean = false
   gameEnd : boolean = false
@@ -16,7 +25,22 @@ export class TrainerNotesComponent
   play : boolean = false
   note : boolean = false
 
-  notes : string[] = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+  notes : Note[] = 
+  [
+    {name : 'C', freq : 261.63 },
+    {name : 'C#', freq : 277.18 },
+    {name : 'D', freq : 293.66 },
+    {name : 'D#', freq : 311.13, },
+    {name : 'E', freq : 329.63 },
+    {name : 'F', freq : 349.23 },
+    {name : 'F#', freq : 369.99 },
+    {name : 'G', freq : 392 },
+    {name : 'G#', freq : 415.3 },
+    {name : 'A', freq : 440 },
+    {name : 'A#', freq : 466.16 },
+    {name : 'B', freq : 493.88 },
+
+  ]
 
   userNote : string = ''
   randomNote : string = ''
@@ -54,12 +78,12 @@ export class TrainerNotesComponent
     this.generateRandomNote()
   }
 
-  generateRandomNote() 
-  {
-    const randomIndex = Math.floor(Math.random() * this.notes.length)
-    this.randomNote = this.notes[randomIndex]
+  generateRandomNote() {
+    const randomIndex = Math.floor(Math.random() * this.notes.length); // Choix aléatoire d'un index
+    const selectedNote = this.notes[randomIndex]; // Récupération de la note aléatoire
+    this.randomNote = selectedNote.name; // Assignation du nom de la note à la propriété randomNote
+    this.playNote(selectedNote.freq); // Jouer la fréquence de la note sélectionnée
   }
-
   checkNote()
   {
     if(this.userNote === this.randomNote)
@@ -70,5 +94,10 @@ export class TrainerNotesComponent
     {
       this.result = 'You Suck !'
     }
+  }
+
+  playNote(freq : number)
+  {
+    this.audioService.playFrequencyWithEnvelope(freq, 1, 1)
   }
 }
