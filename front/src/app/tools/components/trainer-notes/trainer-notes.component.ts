a31d5ca656843e53963a506c13037be94198098d
FlyingPAD <tonyvan@live.fr>
2024-06-06 04:23:50 +0200

    Trainer Fixes

--- front/src/app/tools/components/trainer-notes/trainer-notes.component.ts
+++ front/src/app/tools/components/trainer-notes/trainer-notes.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnDestroy, inject } from '@angular/core';
 import { AudioOldService } from '../../../core/services/audio-old.service';
 
 export class Note
@@ -8,6 +8,7 @@ export class Note
   row : number = 0
   alteration : boolean = false
   extension : boolean = false
+  doubleUp : boolean | undefined = undefined
 }
 
 @Component({
@@ -15,7 +16,7 @@ export class Note
   templateUrl: './trainer-notes.component.html',
   styleUrl: './trainer-notes.component.scss'
 })
-export class TrainerNotesComponent 
+export class TrainerNotesComponent implements OnDestroy
 {
   audioService = inject(AudioOldService)
 
@@ -41,6 +42,26 @@ export class TrainerNotesComponent
   message : string = 'Get Ready !'
 
   notes : Note[] = []
+  notesREF : Note[] = 
+  [
+    { name: 'C', freq: 261.63, row: 1, alteration: false, extension: true, doubleUp : undefined },
+    { name: 'C#', freq: 277.18, row: 1, alteration: true, extension: true, doubleUp : undefined },
+    { name: 'D', freq: 293.66, row: 2, alteration: false, extension: false, doubleUp : undefined },
+    { name: 'D#', freq: 311.13, row: 2, alteration: true, extension: false, doubleUp : undefined },
+    { name: 'E', freq: 329.63, row: 3, alteration: false, extension: false, doubleUp : undefined },
+    { name: 'F', freq: 349.23, row: 4, alteration: false, extension: false, doubleUp : undefined },
+    { name: 'F#', freq: 369.99, row: 4, alteration: true, extension: false, doubleUp : undefined },
+    { name: 'G', freq: 392, row: 5, alteration: false, extension: false, doubleUp : undefined },
+    { name: 'G#', freq: 415.3, row: 5, alteration: true, extension: false, doubleUp : undefined },
+    { name: 'A', freq: 440, row: 6, alteration: false, extension: false, doubleUp : undefined },
+    { name: 'A#', freq: 466.16, row: 6, alteration: true, extension: false, doubleUp : undefined },
+    { name: 'B', freq: 493.88, row: 7, alteration: false, extension: false, doubleUp : undefined },
+  ]
+
+  ngOnDestroy() : void 
+  {
+    this.timerStop()
+  }
 
   reset()
   {
@@ -63,7 +84,7 @@ export class TrainerNotesComponent
     this.previousRandomNote = new Note() 
 
     this.userNote = new Note() 
-    this.message = 'Get ready !'
+    this.message = 'Get Ready !'
   }
 
   start()
@@ -145,17 +166,17 @@ export class TrainerNotesComponent
 
   checkNote()
   {
-    if(this.userNote === this.randomNote)
+    if(this.userNote.name === this.randomNote.name)
     {
       this.timer += 1
-      this.message = 'You Rule !'
+      this.message = 'Yeah ! Keep Going !'
       this.score += 5    
     }
-    if(this.userNote !== this.randomNote)
+    if(this.userNote.name !== this.randomNote.name)
     {
       this.timer -= 2
-      this.message = 'You Suck ... It was ' + this.previousRandomNote.name
-      this.score -= 5 
+      this.message = 'Wrong !! It was ' + this.previousRandomNote.name + ' !!'
+      this.score -= 6 
     }
   }
 
@@ -164,59 +185,75 @@ export class TrainerNotesComponent
     this.audioService.playFrequencyWithEnvelope(freq, 1, 1)
   }
 
-  initializeNotes() {
-    if (this.clefTreble === true) 
-    {
-      this.notes =
-      [
-        { name: 'C', freq: 261.63, row: 1, alteration: false, extension: true },
-        { name: 'C#', freq: 277.18, row: 1, alteration: true, extension: true },
-        { name: 'D', freq: 293.66, row: 2, alteration: false, extension: false },
-        { name: 'D#', freq: 311.13, row: 2, alteration: true, extension: false },
-        { name: 'E', freq: 329.63, row: 3, alteration: false, extension: false },
-        { name: 'F', freq: 349.23, row: 4, alteration: false, extension: false },
-        { name: 'F#', freq: 369.99, row: 4, alteration: true, extension: false },
-        { name: 'G', freq: 392, row: 5, alteration: false, extension: false },
-        { name: 'G#', freq: 415.3, row: 5, alteration: true, extension: false },
-        { name: 'A', freq: 440, row: 6, alteration: false, extension: false },
-        { name: 'A#', freq: 466.16, row: 6, alteration: true, extension: false },
-        { name: 'B', freq: 493.88, row: 7, alteration: false, extension: false },
-      ]
-    }
+  initializeNotes() 
+  {
     if (this.clefBass === true) 
     {
       this.notes =
       [
-        { name: 'C', freq: 130.815, row: 6, alteration: false, extension: false },
-        { name: 'C#', freq: 138.59, row: 6, alteration: true, extension: false },
-        { name: 'D', freq: 146.83, row: 7, alteration: false, extension: false },
-        { name: 'D#', freq: 155.565, row: 7, alteration: true, extension: false },
-        { name: 'E', freq: 164.815, row: 8, alteration: false, extension: false },
-        { name: 'F', freq: 174.615, row: 9, alteration: false, extension: false },
-        { name: 'F#', freq: 184.995, row: 9, alteration: true, extension: false },
-        { name: 'G', freq: 196, row: 10, alteration: false, extension: false },
-        { name: 'G#', freq: 207.65, row: 10, alteration: true, extension: false },
-        { name: 'A', freq: 220, row: 11, alteration: false, extension: false },
-        { name: 'A#', freq: 233.08, row: 11, alteration: true, extension: false },
-        { name: 'B', freq: 246.94, row: 12, alteration: false, extension: false }
+        { name: 'C', freq: 130.815, row: 6, alteration: false, extension: false, doubleUp : undefined },
+        { name: 'C#', freq: 138.59, row: 6, alteration: true, extension: false, doubleUp : undefined },
+        { name: 'D', freq: 146.83, row: 7, alteration: false, extension: false, doubleUp : undefined },
+        { name: 'D#', freq: 155.565, row: 7, alteration: true, extension: false, doubleUp : undefined },
+        { name: 'E', freq: 164.815, row: 8, alteration: false, extension: false, doubleUp : undefined },
+        { name: 'F', freq: 174.615, row: 9, alteration: false, extension: false, doubleUp : undefined },
+        { name: 'F#', freq: 184.995, row: 9, alteration: true, extension: false, doubleUp : undefined },
+        { name: 'G', freq: 196, row: 10, alteration: false, extension: false, doubleUp : undefined },
+        { name: 'G#', freq: 207.65, row: 10, alteration: true, extension: false, doubleUp : undefined },
+        { name: 'A', freq: 220, row: 11, alteration: false, extension: false, doubleUp : undefined },
+        { name: 'A#', freq: 233.08, row: 11, alteration: true, extension: false, doubleUp : undefined },
+        { name: 'B', freq: 246.94, row: 12, alteration: false, extension: false, doubleUp : undefined }
       ]
     }
     if (this.clefAlto === true) 
     {
       this.notes =
       [
-        { name: 'C', freq: 261.63, row: 7, alteration: false, extension: false },
-        { name: 'C#', freq: 277.18, row: 7, alteration: true, extension: false },
-        { name: 'D', freq: 293.66, row: 8, alteration: false, extension: false },
-        { name: 'D#', freq: 311.13, row: 8, alteration: true, extension: false },
-        { name: 'E', freq: 329.63, row: 9, alteration: false, extension: false },
-        { name: 'F', freq: 349.23, row: 10, alteration: false, extension: false },
-        { name: 'F#', freq: 369.99, row: 10, alteration: true, extension: false },
-        { name: 'G', freq: 392, row: 11, alteration: false, extension: false },
-        { name: 'G#', freq: 415.3, row: 11, alteration: true, extension: false },
-        { name: 'A', freq: 440, row: 12, alteration: false, extension: false },
-        { name: 'A#', freq: 466.16, row: 12, alteration: true, extension: false },
-        { name: 'B', freq: 493.88, row: 13, alteration: false, extension: false },
+        { name: 'C', freq: 261.63, row: 7, alteration: false, extension: false, doubleUp : undefined },
+        { name: 'C#', freq: 277.18, row: 7, alteration: true, extension: false, doubleUp : undefined },
+        { name: 'D', freq: 293.66, row: 8, alteration: false, extension: false, doubleUp : undefined },
+        { name: 'D#', freq: 311.13, row: 8, alteration: true, extension: false, doubleUp : undefined },
+        { name: 'E', freq: 329.63, row: 9, alteration: false, extension: false, doubleUp : undefined },
+        { name: 'F', freq: 349.23, row: 10, alteration: false, extension: false, doubleUp : undefined },
+        { name: 'F#', freq: 369.99, row: 10, alteration: true, extension: false, doubleUp : undefined },
+        { name: 'G', freq: 392, row: 11, alteration: false, extension: false, doubleUp : undefined },
+        { name: 'G#', freq: 415.3, row: 11, alteration: true, extension: false, doubleUp : undefined },
+        { name: 'A', freq: 440, row: 12, alteration: false, extension: false, doubleUp : undefined },
+        { name: 'A#', freq: 466.16, row: 12, alteration: true, extension: false, doubleUp : undefined },
+        { name: 'B', freq: 493.88, row: 13, alteration: false, extension: false, doubleUp : undefined },
+      ]
+    }
+    if (this.clefTreble === true) 
+    {
+      this.notes =
+      [
+        { name: 'A', freq: 220, row: -1, alteration: false, extension: true, doubleUp: false },
+        { name: 'A#', freq: 233.08, row: -1, alteration: true, extension: true, doubleUp: false },
+        { name: 'B', freq: 246.94, row: 0, alteration: false, extension: false, doubleUp: true },
+        { name: 'C', freq: 261.63, row: 1, alteration: false, extension: true, doubleUp : undefined },
+        { name: 'C#', freq: 277.18, row: 1, alteration: true, extension: true, doubleUp : undefined },
+        { name: 'D', freq: 293.66, row: 2, alteration: false, extension: false, doubleUp : undefined },
+        { name: 'D#', freq: 311.13, row: 2, alteration: true, extension: false, doubleUp : undefined },
+        { name: 'E', freq: 329.63, row: 3, alteration: false, extension: false, doubleUp : undefined },
+        { name: 'F', freq: 349.23, row: 4, alteration: false, extension: false, doubleUp : undefined },
+        { name: 'F#', freq: 369.99, row: 4, alteration: true, extension: false, doubleUp : undefined },
+        { name: 'G', freq: 392, row: 5, alteration: false, extension: false, doubleUp : undefined },
+        { name: 'G#', freq: 415.3, row: 5, alteration: true, extension: false, doubleUp : undefined },
+        { name: 'A', freq: 440, row: 6, alteration: false, extension: false, doubleUp : undefined },
+        { name: 'A#', freq: 466.16, row: 6, alteration: true, extension: false, doubleUp : undefined },
+        { name: 'B', freq: 493.88, row: 7, alteration: false, extension: false, doubleUp : undefined },
+        { name: 'C', freq: 523.26, row: 8, alteration: false, extension: false, doubleUp : undefined },
+        { name: 'C#', freq: 554.36, row: 8, alteration: true, extension: false, doubleUp : undefined },
+        { name: 'D', freq: 587.32, row: 9, alteration: false, extension: false, doubleUp : undefined },
+        { name: 'D#', freq: 622.26, row: 9, alteration: true, extension: false, doubleUp : undefined },
+        { name: 'E', freq: 659.26, row: 10, alteration: false, extension: false, doubleUp : undefined },
+        { name: 'F', freq: 698.46, row: 11, alteration: false, extension: false, doubleUp : undefined },
+        { name: 'F#', freq: 739.98, row: 11, alteration: true, extension: false, doubleUp : undefined },
+        { name: 'G', freq: 784, row: 12, alteration: false, extension: false, doubleUp : undefined },
+        { name: 'G#', freq: 830.6, row: 12, alteration: true, extension: false, doubleUp : undefined },
+        // { name: 'A', freq: 880, row: 13, alteration: false, extension: true, doubleUp : undefined },
+        // { name: 'A#', freq: 932.32, row: 13, alteration: true, extension: true, doubleUp : undefined },
+        // { name: 'B', freq: 987.76, row: 14, alteration: false, extension: false, doubleUp : true }
       ]
     }
   }
