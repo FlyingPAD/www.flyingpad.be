f178328e04d51704d90059008356af76d3179e28
FlyingPAD <99176621+FlyingPAD@users.noreply.github.com>
2024-06-21 13:17:49 +0200

    Update

--- front-ng18/src/app/features/links/links-list/links-list.component.ts
+++ front-ng18/src/app/features/links/links-list/links-list.component.ts
@@ -2,16 +2,19 @@ import { Component, input } from '@angular/core';
 import { Link } from '../../../models/link';
 import { FilterPipe } from '../../../pipes/links-filter.pipe';
 import { MatListModule } from '@angular/material/list';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
     selector: 'app-links-list',
     standalone: true,
     templateUrl: './links-list.component.html',
     styleUrl: './links-list.component.scss',
-    imports: [FilterPipe, MatListModule]
+    imports: [FilterPipe, MatListModule, NgxPaginationModule]
 })
 export class LinksListComponent 
 {
   links = input.required<Link[]>();
   search = input('');
+  currentPage : number = 1
+  linksPerPage : number = 12
 }
--- front-ng18/src/app/pages/links/links.component.ts
+++ front-ng18/src/app/pages/links/links.component.ts
@@ -1,30 +1,37 @@
-import { Component, inject, signal } from '@angular/core';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { Component, Signal, inject, input, signal } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { map } from 'rxjs';
 import { LinksListComponent } from '../../features/links/links-list/links-list.component';
 import { LinkFilterComponent } from '../../features/links/link-filter/link-filter.component';
 import { CommonModule } from '@angular/common';
+import { LinkCategoriesListComponent } from '../../features/links/link-categories-list/link-categories-list.component';
+import { Link, LinkCategoryLight } from '../../models/link';
 
 @Component({
   selector: 'app-links',
   standalone: true,
-  imports: [CommonModule, RouterModule, LinksListComponent, LinkFilterComponent],
+  imports: [CommonModule, RouterModule, LinksListComponent, LinkFilterComponent, LinkCategoriesListComponent],
   templateUrl: './links.component.html',
   styleUrl: './links.component.scss'
 })
 export class LinksComponent 
 {
-  #activatedRoute = inject(ActivatedRoute);
+  #activatedRoute = inject(ActivatedRoute)
 
-  links = toSignal(this.#activatedRoute.data.pipe(
-    map( (data : any) => data['links']) 
-  ))
-  
-  search = signal('');
+  links = input<Link[]>([])
+  linkCategories = input<LinkCategoryLight[]>([])
+  idParam: Signal<string | undefined>
+  selectedLink = signal<Link | undefined>(undefined)
+  search = signal('')
 
-  handleSearch(value : string) 
-  {
+  constructor() {
+    this.idParam = toSignal(this.#activatedRoute.queryParams.pipe(
+      map(params => params['category'])
+    ))
+  }
+
+  handleSearch(value : string) {
     this.search.set(value)
   }
 }
