5079c21fad70c8f857db7e5a09e26cb648c8b303
FlyingPAD <99176621+FlyingPAD@users.noreply.github.com>
2024-09-11 16:28:19 +0200

    Refactoring

--- back/MB.Application/Features/Franchises/Commands/DeleteFranchise/DeleteFranchiseCommandHandler.cs
+++ back/MB.Application/Features/Franchises/Commands/DeleteFranchise/DeleteFranchiseCommandHandler.cs
@@ -1,21 +1,23 @@
 ﻿using MB.Application.Exceptions;
 using MB.Application.Interfaces.Persistence.Common;
+using MB.Application.Models;
 using MB.Domain.Entities;
 using MediatR;
 
 namespace MB.Application.Features.Franchises.Commands.DeleteFranchise;
 
-public class DeleteFranchiseCommandHandler(IBaseRepository<Franchise> franchiseRepository) : IRequestHandler<DeleteFranchiseCommand, DeleteFranchiseCommandResponse>
+public class DeleteFranchiseCommandHandler(IBaseRepository<Franchise> franchiseRepository) : IRequestHandler<DeleteFranchiseCommand, BaseResponse>
 {
     private readonly IBaseRepository<Franchise> _franchiseRepository = franchiseRepository;
 
-    public async Task<DeleteFranchiseCommandResponse> Handle(DeleteFranchiseCommand request, CancellationToken cancellationToken)
+    public async Task<BaseResponse> Handle(DeleteFranchiseCommand request, CancellationToken cancellationToken)
     {
-        var franchise = await _franchiseRepository.GetByBusinessIdAsync(request.FranchiseId) ?? throw new NotFoundException($"Franchise with ID {request.FranchiseId} was not found.");
+        var franchise = await _franchiseRepository.GetByBusinessIdAsync(request.FranchiseId) 
+                ?? throw new NotFoundException($"Franchise with ID {request.FranchiseId} was not found.");
 
         await _franchiseRepository.DeleteAsync(franchise);
 
-        return new DeleteFranchiseCommandResponse
+        return new BaseResponse
         {
             Success = true,
             Message = "Success."
