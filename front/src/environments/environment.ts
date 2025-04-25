4181c1a8be7c7a5be1de5bd54f101df3f5424ac5
FlyingPAD <99176621+FlyingPAD@users.noreply.github.com>
2025-04-25 13:51:33 +0200

    minor fixes

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     apiBaseUrl: 'https://localhost:7211',
-    appVersion: '0.8.2',
-    translationVersion: '0.0.3'
+    appVersion: '0.8.3',
+    translationVersion: '0.0.4'
 }
