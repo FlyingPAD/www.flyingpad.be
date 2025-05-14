99574ebec989659b3965cbee36a06d47a62b92bf
FlyingPAD <99176621+FlyingPAD@users.noreply.github.com>
2025-05-14 12:18:00 +0200

    Seeding Fix

--- back/MB.Persistence/Migrations/20250514101621_Seeding_Fix.Designer.cs
+++ back/MB.Persistence/Migrations/20250514101621_Seeding_Fix.Designer.cs
@@ -4,6 +4,7 @@ using MB.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MB.Persistence.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20250514101621_Seeding_Fix")]
+    partial class Seeding_Fix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
