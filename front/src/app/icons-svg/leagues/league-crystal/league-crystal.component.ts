807edce6c29dcb322ba9d81ac364069f99a61072
FlyingPAD <99176621+FlyingPAD@users.noreply.github.com>
2025-06-02 06:41:33 +0200

    Various Fixes

--- front/src/app/icons-svg/leagues/league-crystal/league-crystal.component.ts
+++ front/src/app/icons-svg/leagues/league-crystal/league-crystal.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-league-crystal',
   templateUrl: './league-crystal.component.html'
 })
 export class LeagueCrystalComponent {
-
+  @Input() size: string = '30px'
 }
--- front/src/app/icons-svg/leagues/league-wood/league-wood.component.ts
+++ front/src/app/icons-svg/leagues/league-wood/league-wood.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-league-wood',
   templateUrl: './league-wood.component.html'
 })
 export class LeagueWoodComponent {
-
+  @Input() size: string = '30px'
 }
